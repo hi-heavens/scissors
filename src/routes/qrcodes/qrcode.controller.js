98d1fcdff369fe71c41a4140cb636ecbd823a174
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-06-21 17:53:52 +0100

    implement a check if the message exist on the DB and returned it duly

--- src/routes/qrcodes/qrcode.controller.js
+++ src/routes/qrcodes/qrcode.controller.js
@@ -6,22 +6,31 @@ import QRCode from "qrcode";
 export const generateQRCode = catchAsync(async function (req, res, next) {
   const { message } = req.body;
   if (!message) {
-    return next(new AppError("Please provide a message to code", 400));
+    return next(new AppError("Please provide a message/link to code", 400));
   }
-  // Generate QR code as Blob
-  const qrCodeBlob = await QRCode.toBuffer(message, {
-    width: 300,
-    height: 300,
+
+  let qrCodeBlob;
+  // Check if QR code already exists
+  const findQRCode = await qrCode.findOne({ message });
+  if (findQRCode) {
+    qrCodeBlob = findQRCode.qrCodeBlob;
+  } else {
+    // Generate QR code as Blob
+    qrCodeBlob = await QRCode.toBuffer(message, {
+      width: 300,
+      height: 300,
+    });
+    // Save QR code in MongoDB
+    const code = new qrCode({
+      message,
+      qrCodeBlob,
+    });
+    await code.save();
+  }
+  res.set({
+    "Content-Disposition": 'attachment; filename="qrcode.png"',
+    "Content-Type": "image/png",
   });
-  // Save QR code in MongoDB
-  const code = new qrCode({
-    message,
-    qrCodeBlob,
-  });
-  await code.save();
-  
-  res.set("Content-Disposition", 'attachment; filename="qrcode.png"');
-  res.set("Content-Type", "image/png");
   console.log("QR Code generated successfully");
   res.send(qrCodeBlob);
 });
