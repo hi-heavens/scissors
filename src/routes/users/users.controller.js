cbaa8729dc6d3533e43e6f4071984e32f849b281
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-06-21 06:24:27 +0100

    did some error handling, env declare, update error response on user controller

--- src/routes/users/users.controller.js
+++ src/routes/users/users.controller.js
@@ -1,85 +1,75 @@
 import validator from "validator";
 import { User } from "../../models/user.model.js";
 import { getToken, decodeToken } from "../../utils/token.utils.js";
-import  catchAsync  from "../../utils/catchAsync.error.js";
+import catchAsync from "../../utils/catchAsync.error.js";
+import { AppError } from "../../utils/app.error.js";
 
 export const validateUser = catchAsync(async function (req, res, next) {
-    const requestHandler = req.headers.authorization;
+  const requestHandler = req.headers.authorization;
 
-    const decode = await decodeToken(requestHandler);
-    const loginUser = await User.findById(decode.id);
-    if (!loginUser) {
-        return next(new AppError("The user could not be found", 401));
-    }
-    req.user = loginUser;
-    next();
+  const decode = await decodeToken(requestHandler);
+  const loginUser = await User.findById(decode.id);
+  if (!loginUser) {
+    return next(new AppError("The user could not be found", 401));
+  }
+  req.user = loginUser;
+  next();
 });
 
-async function signUp(req, res, next) {
-    const { user_name, email, password } = req.body;
+export const signUp = catchAsync(async function (req, res, next) {
+  const { user_name, email, password } = req.body;
 
-    if (!user_name || !email || !password) {
-        return res.status(401).json({
-            status: "failed",
-            message: "Kindly reconfirm registration details and try again"
-        })
-    }
-    const user = new User({
-        user_name,
-        email,
-        password
-    });
-    const newUser = await user.save();
+  if (!user_name || !email || !password) {
+    return next(new AppError("Kindly reconfirm registration details and try again.", 401));
+  }
+  const user = new User({
+    user_name,
+    email,
+    password,
+  });
+  const newUser = await user.save();
 
-    const token = getToken(newUser._id);
+  const token = getToken(newUser._id);
 
-    res.status(201).json({
-        status: "successfully registered",
-        token,
-        data: {
-            userId: newUser.id,
-            userName: newUser.user_name,
-            email: newUser.email,
-            userName: newUser.user_name,
-        }
-    });
- }
+  res.status(201).json({
+    status: "successfully registered",
+    token,
+    data: {
+      userId: newUser.id,
+      userName: newUser.user_name,
+      email: newUser.email,
+      userName: newUser.user_name,
+    },
+  });
+});
 
-async function login(req, res, next) {
-    const { loginInput, password } = req.body;
-    
-    if (!loginInput || !password) {
-        return res.status(400).json({
-            status: "failed",
-            message: "Please provide a valid username/email and/or password."
-        })
-    }
-    
-    let user;
-    if (validator.isEmail(loginInput)) {
-        user = await User.findOne({email: loginInput}).select("+password");
-    } else {
-        user = await User.findOne({user_name: loginInput}).select("+password");
-    }
+export const login = catchAsync(async function (req, res, next) {
+  const { loginInput, password } = req.body;
 
-    if (!user || !(await user.validPassword(password, user.password))) {
-        return res.status(401).json({
-            status: "failed",
-            message: "Incorrect username/email and/or password"
-        })
-    }
+  if (!loginInput || !password) {
+    return next(new AppError("Please provide a valid username/email and/or password.", 400));
+  }
 
-    let token = getToken(user._id);
-    res.status(200).json({
-        status: "login success",
-        token,
-        data: {
-            userId: user.id,
-            userName: user.user_name,
-            email: user.email,
-            userName: user.user_name,
-        }
-    })
-};
+  let user;
+  if (validator.isEmail(loginInput)) {
+    user = await User.findOne({ email: loginInput }).select("+password");
+  } else {
+    user = await User.findOne({ user_name: loginInput }).select("+password");
+  }
 
-export { signUp, login };
+  if (!user || !(await user.validPassword(password, user.password))) {
+    return next(new AppError("Incorrect username/email and/or password", 401));
+  }
+
+  let token = getToken(user._id);
+  res.status(200).json({
+    status: "login success",
+    token,
+    data: {
+      userId: user.id,
+      userName: user.user_name,
+      email: user.email,
+      userName: user.user_name,
+    },
+  });
+});
--- src/utils/globalErrorHandler.js
+++ src/utils/globalErrorHandler.js
@@ -1,8 +1,64 @@
-export default (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error! 🔥";
+import { AppError } from "./app.error.js";
+
+const sendErrorDev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
+      status: err.status,
+      message: err.message,
     });
+  } else {
+    console.error("ERROR 💥", err);
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
+export default (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error! 🔥";
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(err);
+
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(err, res);
+  }
 };
